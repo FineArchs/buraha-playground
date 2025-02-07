0a310cb567a2fb31fee8d5348fc2104ce959bd7e
FineArchs <cicada1003@gmail.com>
2025-02-07 15:57:48 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div>paste a hash:</div>
+<input v-model="hash" placeholder="hash" />
+<div class='spacer' />
+<Buraha v-if="hash" class="buraha" :hash="hash" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Buraha from './components/Buraha.vue';
+
+const hash = ref('');
+</script>
+
+<style scoped>
+input {
+	width: 100%;
+}
+.buraha {
+	display: block;
+}
+.spacer {
+	height: 2em;
+}
+</style>
---
+++ src/components/Buraha.vue
@@ -0,0 +1,15 @@
+<template>
+<canvas ref="canvas" />
+</template>
+
+<script lang="ts" setup>
+import { render } from 'buraha';
+import { shallowRef, watch } from 'vue';
+
+const props = defineProps<{
+	hash: string;
+}>();
+const canvas = shallowRef<HTMLCanvasElement>();
+
+watch([canvas, () => props.hash], () => canvas.value && render(props.hash, canvas.value));
+</script>
